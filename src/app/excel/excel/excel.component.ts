b3e5e7572b9752c20abc58eaa5bb5a33d09b5059
Ajay <ajay.anthony@aspiresys.com>
2020-01-03 16:21:57 +0530

    ecel

--- src/app/excel/excel/excel.component.ts
+++ src/app/excel/excel/excel.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as XLSX from 'xlsx'
 import {AngularFirestore} from '@angular/fire/firestore'
+import { firestore } from 'firebase';
 
 @Component({
   selector: 'app-excel',
@@ -19,14 +20,148 @@ private firestore : AngularFirestore
   formValues:any
   rcNumber=''
   href=''
+  select=true;
+  loading= false;
+  loadedData = false;
   workcentre=''
   filesize=''
-  sheetNames=[];
+  sheetName=0;
+   workbook = null;
+  sheetArray=[];
   fileName=''
   imgarray:string[]=[]
   another:any[]=[]
 
   ngOnInit() {
+  }
+
+  chooseSheet(){
+    console.log(this.sheetName)
+    const sheet=this.workbook.Sheets[this.workbook.SheetNames[this.sheetName]]
+
+    console.log(sheet)
+    var range= XLSX.utils.decode_range(sheet['!ref'])
+    console.log(range)
+    this.traverseColumn(range,sheet)
+    
+    
+  }
+
+ saveData(){
+
+  this.loading = true;
+
+  this.another.forEach(async (item,index)=>{
+
+console.log(item,index)   
+console.log(item.RcNumber) 
+ await this.firestore.collection('values').doc(item.RcNumber).set(item).then(()=>{
+        console.log('succ')
+        this.firestore.collection('details').doc('WC1').update({id: firestore.FieldValue.arrayUnion(item.RcNumber)}).then(()=>{
+          console.log('entered')
+        })
+      })
+  })
+this.loading=false;
+  }
+
+  async traverseColumn (range,sheet){
+    this.loading= true;
+    this.loadedData=false;
+      // traversing through the columns
+   this.another=[];
+    
+      for(var R = range.s.r+1; R <= range.e.r; ++R) {
+        var skip = false;
+        for(var C = range.s.c; C <= range.e.c; ++C) {
+        var value;
+        this.workcentre=''
+      
+        
+    
+         
+          var cell_address = {c:C, r:R};
+        
+       //  console.log(this.headingArray[C])
+           var cell_ref = XLSX.utils.encode_cell(cell_address);
+
+           if(C<9 && !sheet[cell_ref])
+           {
+             console.log('aj')
+             skip = true;
+
+             break;
+           }
+
+          if(!sheet[cell_ref] && C>=9)
+          {
+            value=false;
+          //  console.log(cell_ref +'-->'+ 'false')
+          }
+          else if(C>=9 && sheet[cell_ref]){
+              value= true;
+          }
+          else {
+            value= sheet[cell_ref].v
+           // console.log(cell_ref +'-->'+ sheet[cell_ref].v)
+          }
+          
+          var temp ={};
+          temp[this.headingArray[C]] = value;
+         
+          this.formValues = {...this.formValues,...temp}
+             
+        }
+        console.log(skip)
+        if(!skip)
+        {
+          if(this.formValues.BEMCO)
+          {
+          this.workcentre = this.workcentre.concat('BEMCO ')
+         // console.log(this.workcentre)
+          }
+          if(this.formValues.HYD)
+          {
+            this.workcentre = this.workcentre.concat('HYD ')
+           // console.log(this.workcentre)
+          }
+          if(this.formValues.HAB)
+          {
+            this.workcentre = this.workcentre.concat('HAB ')
+            //console.log(this.workcentre)
+          }
+          var flag = {};
+          flag['workcentre'] = this.workcentre;
+          temp['finish']=false;
+          
+          this.formValues={...this.formValues,...flag,...temp}
+         
+          console.log(this.formValues.RcNumber)
+          this.rcNumber= this.formValues.RcNumber
+          await this.sleep();
+          this.href=document.getElementsByTagName('img')[0].src;
+          console.log('href')
+          flag['imagehref']= this.href
+          this.formValues = {...this.formValues,...flag}
+          //this.imgarray.push(this.href)
+          console.log(this.formValues)
+          this.another.push(this.formValues)
+        
+        }
+
+        
+      }
+     
+      console.log (this.another)
+      
+      this.loading=false;
+      this.loadedData=true;
+      
+     
+      
+    
+    
+     
   }
 
   bytesToSize(bytes:any) {
@@ -51,10 +186,12 @@ private firestore : AngularFirestore
     
  }
 
+
+
  onSubmit(ev){
 //console.log(ev)
 
-let workbook = null;
+
 let jsonData = null;
 
 
@@ -70,107 +207,36 @@ const extension = file.name.split('.')[1]
  console.log(file.size)
  this. filesize = this.bytesToSize(parseInt(file.size))
  console.log(this.filesize)
- if(extension == 'xlsx' || extension == 'xls')
+ if(extension === 'xlsx' || extension === 'xls')
  {
+
+    reader.onload = async (event) =>{
+    //console.log(event)
+      const data = reader.result;
+      //console.log(data)
+     this.workbook = XLSX.read(data, {type:'binary'})
+      console.log(this.workbook.SheetNames)
+      this.sheetArray=[];
+      for(var i=0;i<this.workbook.SheetNames.length;i++)
+      {
+        this.sheetArray.push({index: i, name: this.workbook.SheetNames[i]});
+      }
+     
+      this.select = false;
+      console.log(this.sheetName)
+
+    
+     
+    }
+    
+    reader.readAsBinaryString(file);
 
  }
 
  else{
 
  }
-// reader.onload = async (event) =>{
-// //console.log(event)
-//   const data = reader.result;
-//   //console.log(data)
-//   workbook = XLSX.read(data, {type:'binary'})
-//   console.log(workbook.SheetNames)
-//   console.log(workbook.SheetNames.length)
-//   const sheet= workbook.Sheets[workbook.SheetNames[0]]
-//   console.log(sheet)
-//   var range= XLSX.utils.decode_range(sheet['!ref'])
-//   console.log(range)
 
-// // traversing through the columns
-
-//   for(var R = range.s.r+1; R <= range.e.r; ++R) {
-//     for(var C = range.s.c; C <= range.e.c; ++C) {
-//     var value;
-//     this.workcentre=''
-
-     
-//       var cell_address = {c:C, r:R};
-    
-//    //  console.log(this.headingArray[C])
-//        var cell_ref = XLSX.utils.encode_cell(cell_address);
-//       if(!sheet[cell_ref])
-//       {
-//         value=false;
-//       //  console.log(cell_ref +'-->'+ 'false')
-//       }
-//       else if(C>=9 && sheet[cell_ref]){
-//           value= true;
-//       }
-//       else {
-//         value= sheet[cell_ref].v
-//        // console.log(cell_ref +'-->'+ sheet[cell_ref].v)
-//       }
-      
-//       var temp ={};
-//       temp[this.headingArray[C]] = value;
-     
-//       this.formValues = {...this.formValues,...temp}
-         
-//     }
-    
-//     if(this.formValues.BEMCO)
-//     {
-//     this.workcentre = this.workcentre.concat('BEMCO ')
-//     console.log(this.workcentre)
-//     }
-//     if(this.formValues.HYD)
-//     {
-//       this.workcentre = this.workcentre.concat('HYD ')
-//       console.log(this.workcentre)
-//     }
-//     if(this.formValues.HAB)
-//     {
-//       this.workcentre = this.workcentre.concat('HAB ')
-//       console.log(this.workcentre)
-//     }
-//     var flag = {};
-//     flag['workcentre'] = this.workcentre;
-//     temp['finish']=false;
-    
-//     this.formValues={...this.formValues,...flag,...temp}
-   
-//     console.log(this.formValues.RcNumber)
-//     this.rcNumber= this.formValues.RcNumber
-//     await this.sleep();
-//     this.href=document.getElementsByTagName('img')[0].src;
-//     console.log('href')
-//     flag['imagehref']= this.href
-//     this.formValues = {...this.formValues,...flag}
-//     this.imgarray.push(this.href)
-//     console.log(this.formValues)
-//     this.another.push(this.formValues)
-  
-//   }
- 
-  
-  
-  
- 
-  
-
-
-//   // this.firestore.collection('det').doc(this.another[1].RcNumber).set(this.another[1]).then(()=>{
-//   //   console.log('succ')
-//   // })
-
- 
-// }
-
-// reader.readAsBinaryString(file);
 
   }
 
